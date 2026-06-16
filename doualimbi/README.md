e03e6f573d10bff66fce4b4f11e1a8c5361bb1a6
PavelIanisEmanuel <pavel.ianis.emanuel@gmail.com>
2026-06-16 21:55:55 +0200

    deleted a clone of the whole project created by accident

--- doualimbi/README.md
+++
@@ -1,2 +0,0 @@
-# douălimbi
-WMC SS project
--- doualimbi/backend.js
+++
@@ -1,9 +0,0 @@
-//MATIAS BACKEND; CREATE JSON SERVER
-//functions for fetching or writing data to the json server
-//useful for login.js
-
-class Server {
-    fetch () {}
-
-    write () {}
-}
